44be9d1bde7ea98c21111fc8109219f99166af5b
KWarp11 <KWarp11@e36260e9-b1c7-7c20-eb4b-380553df3b3b>
2010-04-25 10:13:00 +0000

    ported working opengl code to the project next will work on cube class

---
+++ trunk/CS480_HW3/Cube.h
@@ -0,0 +1,52 @@
+//
+//  Cube.h
+//  GLGravity
+//
+//  Created by Greg Lieberman on 4/25/10.
+//  Copyright 2010 Greg Lieberman. All rights reserved.
+//
+
+#ifndef CUBE_H
+#define CUBE_H
+
+//const int num_teapot_indices = 2781;
+//const int num_teapot_vertices = 1364;
+//const int num_teapot_normals = 1364;
+
+const float cubeVertices[] = {
+	// FRONT
+	-0.5f, -0.5f,  0.5f,
+	0.5f, -0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
+	0.5f,  0.5f,  0.5f,
+	// BACK
+	-0.5f, -0.5f, -0.5f,
+	-0.5f,  0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f,
+	0.5f,  0.5f, -0.5f,
+	// LEFT
+	-0.5f, -0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f,  0.5f, -0.5f,
+	// RIGHT
+	0.5f, -0.5f, -0.5f,
+	0.5f,  0.5f, -0.5f,
+	0.5f, -0.5f,  0.5f,
+	0.5f,  0.5f,  0.5f,
+	// TOP
+	-0.5f,  0.5f,  0.5f,
+	0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f, -0.5f,
+	0.5f,  0.5f, -0.5f,
+	// BOTTOM
+	-0.5f, -0.5f,  0.5f,
+	-0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f,  0.5f,
+	0.5f, -0.5f, -0.5f,
+	
+	
+};
+
+#endif // CUBE_H
+
