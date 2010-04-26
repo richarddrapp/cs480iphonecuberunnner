3940e920169ec537ba9aa5f6300de74d616a2516
iamrecognized <iamrecognized@e36260e9-b1c7-7c20-eb4b-380553df3b3b>
2010-04-26 05:16:16 +0000

    Particle Again

---
+++ trunk/CS480_HW3/Classes/ParticleController.h
@@ -0,0 +1,20 @@
+//
+//  ParticleController.h
+//  CS480_HW3
+//
+//  Created by Richard Rapp on 4/25/10.
+//  Copyright 2010 University of Southern California. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ParticleController : NSObject {
+	NSMutableArray *explosions;
+}
+
+- (id) init;
+
+
+
+@end
