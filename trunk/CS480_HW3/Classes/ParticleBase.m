b425d90140b9b17504ffe7daa9aab3c7e6fd0b8e
iamrecognized <iamrecognized@e36260e9-b1c7-7c20-eb4b-380553df3b3b>
2010-04-26 05:17:32 +0000

    Particle Again

---
+++ trunk/CS480_HW3/Classes/ParticleBase.m
@@ -0,0 +1,45 @@
+//
+//  ParticleBase.m
+//  CS480_HW3
+//
+//  Created by Richard Rapp on 4/25/10.
+//  Copyright 2010 University of Southern California. All rights reserved.
+//
+
+#import "ParticleBase.h"
+
+
+@implementation ParticleBase
+
+@synthesize x;
+@synthesize y;
+@synthesize z;
+@synthesize live;
+
+- (ParticleBase*) initAt:(float)_x :(float)_y :(float)_z :(int)_particles {
+	self = [super init];
+	x = _x;
+	y + _y;
+	z = _z;
+	
+	live = YES;
+	
+	particles = _particles;
+	
+	parts = [[NSMutableArray alloc] init];
+	
+	for (int i = 0; i < particles; i++) {
+		[parts addObject: [[ParticleCube alloc] init]];
+		[[parts objectAtIndex:i] allocate:x :y :z :.5f :10];
+	}
+	return self;
+}
+
+- (void) updateAndDraw {
+	for (int i = 0; i < particles; i++) {
+		[[parts objectAtIndex:i] updateParticle];
+		[[parts objectAtIndex:i] drawParticle];
+	}
+}
+
+@end
