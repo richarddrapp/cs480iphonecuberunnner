47c372f6208a926b08dfb1232ccf7a783986e9b3
iamrecognized <iamrecognized@e36260e9-b1c7-7c20-eb4b-380553df3b3b>
2010-04-26 05:16:27 +0000

    Particle Again

---
+++ CS480_HW3/Classes/ParticleCube.h
@@ -0,0 +1,51 @@
+//
+//  ParticleCube.h
+//  CS480_HW3
+//
+//  Created by Richard Rapp on 4/25/10.
+//  Copyright 2010 University of Southern California. All rights reserved.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <stdlib.h>
+#import <math.h>
+
+
+@interface ParticleCube : NSObject {
+	float x;
+	float y;
+	float z;
+	
+	float xSpeed;
+	float ySpeed;
+	float zSpeed;
+
+	float DRAG;
+	
+	int life;
+	int maxLife;
+	
+	bool visible;
+}
+
+@property float x;
+@property float y;
+@property float z;
+
+@property bool visible;
+
+// constructor
+- (ParticleCube*) initParticle;
+
+// allocator function
+- (void) allocate: (float) _x : (float) _y : (float) _z : (float) _speed : (int) _life;
+
+
+- (void) updateParticle;
+- (void) drawParticle;
+
+@end
