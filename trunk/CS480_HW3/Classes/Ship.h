1179b76641b2f3d594a5a4ed86efb821558a505b
abghavami <abghavami@e36260e9-b1c7-7c20-eb4b-380553df3b3b>
2010-04-26 03:11:19 +0000

    added my Ship model -- might have some issues in the draw -- haven't tested yet

---
+++ trunk/CS480_HW3/Classes/Ship.h
@@ -0,0 +1,38 @@
+//
+//  Ship.h
+//  CS480_HW3
+//
+//  Created by Anthony Ghavami on 4/25/10.
+//  Copyright 2010 USC. All rights reserved.
+//
+
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+
+@interface Cube : NSObject {
+	//x, y, z properties don't do anything right now
+	//to make them work
+	//the cube needs to store it's own worldTransform matrix
+	// does openGL handle that? I think it does. Investigate.
+	
+	//a possible hack is for each instance to store its own modified vertex coordinates
+	//and re-build the modified vertex coordinates every time x, y, or z is set
+	
+	float x;
+	float y;
+	float z;
+	float scale;
+	
+}
+
+@property float x;
+@property float y;
+@property float z;
+@property float scale;
+
+- (void) drawCube;
+
+
+@end
+
